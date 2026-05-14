e5bb5b39504f1b0b08ca122ea93760e992b00998
2830972513028 <2830972513028>
2026-05-13 21:45:28 -0300

    feat: pipeline CI/CD com CodeQL

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
+import sqlite3
+
+
+def busca_usuario(nome):
+    conexao = sqlite3.connect("banco.db")
+    cursor = conexao.cursor()
+
+    query = f"SELECT * FROM usuarios WHERE nome = '{nome}'"
+
+    cursor.execute(query)
+
+    return cursor.fetchall()
+
+
 def soma(a, b):
     return a + b
-
-
-def executa_codigo(codigo):
-    return eval(codigo)
 
 
 if __name__ == "__main__":
