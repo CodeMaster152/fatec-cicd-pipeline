d2c30b990ca9945d15f8d58481e18d4dbe2c84aa
2830972513028 <2830972513028>
2026-05-13 21:41:25 -0300

    feat: pipeline CI/CD com CodeQL

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ def soma(a, b):
     return a + b
 
 
+def executa_codigo(codigo):
+    return eval(codigo)
+
+
 if __name__ == "__main__":
     resultado = soma(2, 3)
     print(f"Resultado: {resultado}")
