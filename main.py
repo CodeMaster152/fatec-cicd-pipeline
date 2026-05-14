19680ebd3512f84f0aac8df9a578a3e1d06f2620
2830972513028 <2830972513028>
2026-05-13 21:49:44 -0300

    feat: pipeline CI/CD com CodeQL

--- main.py
+++ main.py
@@ -1,21 +1,2 @@
-import sqlite3
-
-
-def busca_usuario(nome):
-    conexao = sqlite3.connect("banco.db")
-    cursor = conexao.cursor()
-
-    query = f"SELECT * FROM usuarios WHERE nome = '{nome}'"
-
-    cursor.execute(query)
-
-    return cursor.fetchall()
-
-
-def soma(a, b):
+def soma(a, b)
     return a + b
-
-
-if __name__ == "__main__":
-    resultado = soma(2, 3)
-    print(f"Resultado: {resultado}")
